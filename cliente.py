b6c219103c818e45fdfd74440a3ea7913ba9e1d4
Gabriel <gabrielmvoga@gmail.com>
2025-07-13 09:11:55 -0300

    Modify Implementação de Cliente e Conta

--- cliente.py
+++ cliente.py
@@ -1,17 +1,46 @@
+from transacao import Deposito, Saque
+
 class Cliente:
-    def __init__(self, endereco, contas=[]):
+    def __init__(self, endereco: str, contas: list = []):
         self._endereco = endereco
         self._contas = contas
 
-    def realizar_transacao():
-        pass
+    def realizar_transacao(self, conta, transacao):
+        if not conta in self._contas:
+            print("Conta inválida")
+            return
+        
+        if isinstance(transacao, Deposito):           
+            if conta.depositar(transacao._valor):
+                transacao.registrar(conta=conta)
+            
+        elif isinstance(transacao,Saque):
+            if conta.sacar(transacao._valor):
+                transacao.registrar(conta)
+        else:
+            print("Transação inválida")
 
-    def adicionar_conta():
-        pass
+    def adicionar_conta(self, conta):
+        self._contas.append(conta)
+
+    def __str__(self):
+        return self.__dict__
 
 class PessoaFisica(Cliente):
-    def __init__(self, cpf, nome, data_nascimento, endereco, contas=[]):
-        super().__init__(endereco, contas)
+    def __init__(self, cpf: str, nome: str, data_nascimento: str, 
+                 endereco: str, contas: list = []):
         self._cpf = cpf
         self._nome = nome
         self._data_nascimento = data_nascimento
+        super().__init__(endereco, contas)
+
+    def __str__(self):
+        return f'{self.__class__.__name__}: {self.__dict__}'
+    
+
+
+
+
+
+
+
--- conta.py
+++ conta.py
@@ -1,29 +1,49 @@
+from historico import Historico
+
 class Conta:
-
+    
     AGENCIA = "0001"
+    nro_conta = 1
 
-
-    def __init__(self, numero, cliente, saldo = 0, historico=[]):
+    def __init__(self, saldo: float = 0, numero: int = None, 
+                 cliente = None, historico=Historico()):
         self._numero = numero
         self._cliente = cliente
         self._saldo = saldo
         self._historico = historico
 
     @property
-    def saldo(self):
+    def saldo(self) -> float:
         return self._saldo
     
-    def nova_conta(cliente, numero):
+    @classmethod
+    def nova_conta(cls, cliente, numero: int):
+        return cls(cliente=cliente, numero=numero)
+
+    def sacar(self, valor: float) -> bool:
         pass
 
-    def sacar(self, valor):
-        pass
-
-    def depositar(self, valor):
+    def depositar(self, valor: float) -> bool:
         pass
 
 class ContaCorrente(Conta):
-    def __init__(self, limite, limite_saques, numero, cliente, saldo=0, historico=[]):
-        super().__init__(numero, cliente, saldo, historico)
+    def __init__(self, limite: float = 500, limite_saques: int = 3, 
+                 numero: int = None, cliente = None, 
+                 saldo: float = 0, historico = Historico()):
+        
+        super().__init__(numero=numero, cliente=cliente, saldo=saldo, 
+                         historico=historico)
         self._limite = limite
         self._limite_saques = limite_saques
+
+    def sacar(self, valor: float) -> bool:
+        self._saldo -= valor
+        return True 
+        # TODO: fazer validações
+
+    def depositar(self, valor: float) -> bool:
+        self._saldo += valor
+        return True
+
+    def __str__(self):
+        return f'{self.__class__.__name__}: {self.__dict__}'
